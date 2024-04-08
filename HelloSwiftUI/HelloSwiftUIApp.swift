45e9a782818a44df970d299a28f3dcb7ef6d0215
SeongKookKIM <ksk8646@naver.com>
2024-04-08 09:26:03 +0900

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by SeongKook on 2024/04/08.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
