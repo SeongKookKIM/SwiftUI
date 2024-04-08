6b7e0ef9bc01d5692881efaaab142ea10d8f05f5
SeongKookKIM <ksk8646@naver.com>
2024-04-08 13:23:28 +0900

    VStack 간격 정의

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     @State private var sliderValue = 0.5
     
     var body: some View {
-        VStack {
+        VStack(alignment: .trailing, spacing: 24) {
             Text("Default padding of 16 ponint")
                 .padding(16)
                 .background(Color.yellow)
