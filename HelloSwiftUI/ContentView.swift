7f789e2fbe3b2bd9026d623cef46583794f54873
SeongKookKIM <ksk8646@naver.com>
2024-04-08 16:36:47 +0900

    Cutom Color

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,7 +7,12 @@
 
 import SwiftUI
 
+extension Color {
+    static let myCustomColor = Color(red:0.5, green: 0.8, blue: 0.3)
+}
+
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             /* 이미지 크기 조정
@@ -55,21 +60,44 @@ struct ContentView: View {
              .overlay(Circle().stroke(Color.purple, lineWidth: 20))
              */
             
-            // 이미지 투명도
-            Image("sample_dog_square")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .shadow(color: Color(hue: 0.241, saturation: 0.389, brightness: 0.948), radius: 50, x: 0, y: 0)
-                .opacity(0.75)
-                
+            /* 이미지 투명도
+             Image("sample_dog_square")
+             .resizable()
+             .frame(width: 250, height: 250)
+             .aspectRatio(contentMode: .fill)
+             .shadow(color: Color(hue: 0.241, saturation: 0.389, brightness: 0.948), radius: 50, x: 0, y: 0)
+             .opacity(0.75)
+             
+             
+             Image("sample_cat_square")
+             .resizable()
+             .frame(width: 250, height: 250)
+             .aspectRatio(contentMode: .fill)
+             .clipShape(Circle())
+             .opacity(0.25)
+             */
             
-            Image("sample_cat_square")
+            //            커스텀 이미지 색상
+            // Assets Color Scheme 사용
+            Image(systemName: "heart.fill")
                 .resizable()
-                .frame(width: 250, height: 250)
-                .aspectRatio(contentMode: .fill)
-                .clipShape(Circle())
-                .opacity(0.25)
+                .aspectRatio( contentMode: .fit)
+                .foregroundStyle(Color("custom-color"))
+            
+            // 지역상수 사용
+            let customColor = Color(red:1, green: 0, blue:0)
+            Image("heart.fill")
+                .resizable()
+                .aspectRatio( contentMode: .fit)
+                .foregroundStyle(customColor)
+            
+            // 전역상수 사용 - extention
+            Image("heart.fill")
+                .resizable()
+                .aspectRatio( contentMode: .fit)
+                .foregroundStyle(Color.myCustomColor)
+            
+            
         }
     }
 }
