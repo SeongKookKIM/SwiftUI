3f1a1826480d36ddade20515880f934500fe86f9
SeongKookKIM <ksk8646@naver.com>
2024-04-08 17:14:45 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,37 +10,11 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var colorMe = false
+
     
     var body: some View {
         VStack {
-            Rectangle()
-                .fill(colorMe ? Color.green : Color.gray)
-                .frame(width: 250, height: 100)
-            
-            Button("여기를 클릭하세여!") {
-                colorMe.toggle()
-            }
-            
-            Button {
-                colorMe.toggle()
-            } label: {
-                Text("Click Here")
-                    .font(.largeTitle)
-                    .foregroundColor(.green)
-                    .padding()
-                    .border(Color.red, width: 6)
-            }
-            
-            Button {
-                colorMe.toggle()
-            } label: {
-                Image("sample_dog_square")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
-            }
+          
         }
     }
 }
