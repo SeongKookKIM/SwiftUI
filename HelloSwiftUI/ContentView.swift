7c383212ed069486a32a905933ebb0681c9651e4
SeongKookKIM <ksk8646@naver.com>
2024-04-08 17:10:17 +0900

    Button Toggle

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,16 +10,20 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
+    @State var colorMe = false
     
     var body: some View {
         VStack {
+            Rectangle()
+                .fill(colorMe ? Color.green : Color.gray)
+                .frame(width: 250, height: 100)
+            
             Button("여기를 클릭하세여!") {
-                print("클릭시 실행코드1")
+                colorMe.toggle()
             }
             
             Button {
-                print("클릭시 실행코드2")
+                colorMe.toggle()
             } label: {
                 Text("Click Here")
                     .font(.largeTitle)
@@ -29,7 +33,7 @@ struct ContentView: View {
             }
             
             Button {
-                print("클릭시 실행코드3")
+                colorMe.toggle()
             } label: {
                 Image("sample_dog_square")
                     .resizable()
