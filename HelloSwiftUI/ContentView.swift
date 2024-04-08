dbd90cce1830f8116776b6495c4bc34e287acee8
SeongKookKIM <ksk8646@naver.com>
2024-04-08 17:00:09 +0900

    Button @ViewBuilder

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,9 +11,32 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
     var body: some View {
         VStack {
-          
+            Button("여기를 클릭하세여!") {
+                print("클릭시 실행코드1")
+            }
+            
+            Button {
+                print("클릭시 실행코드2")
+            } label: {
+                Text("Click Here")
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+                    .border(Color.red, width: 6)
+            }
+            
+            Button {
+                print("클릭시 실행코드3")
+            } label: {
+                Image("sample_dog_square")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.yellow, lineWidth: 4))
+            }
         }
     }
 }
