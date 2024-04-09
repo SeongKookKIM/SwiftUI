eba0cb523b149cb4c85672b69d62bcb5b535ed12
SeongKookKIM <ksk8646@naver.com>
2024-04-09 10:42:41 +0900

    Picker

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,10 +11,28 @@ import SwiftUI
 
 
 struct ContentView: View {
-
+    @State private var choice = 0.0
+    @State private var myColor = Color.red
+    @State var myDate = Date.now
+    
     var body: some View {
         VStack {
+            Picker(selection: $choice, label: Text("Picker")) {
+                Text("Bird").tag(1.7)
+                Text("Cat").tag(2.06)
+                Text("Lizard").tag(3.41)
+                Text("Dog").tag(4.13)
+                Text("Hamster").tag(5.28)
+            }.pickerStyle(SegmentedPickerStyle())
+            Text("You choose \(choice)")
             
+            ColorPicker("Pick a coor", selection: $myColor)
+            Rectangle()
+                .frame(width: 200, height: 150)
+                .foregroundColor(myColor)
+            
+            DatePicker(selection: $myDate, label: { Text("Date")})
+                .datePickerStyle(.graphical)
         }
     }
 }
