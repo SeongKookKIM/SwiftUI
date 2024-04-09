0398a4ec70c71e17d27e10f395764be6296e7509
SeongKookKIM <ksk8646@naver.com>
2024-04-09 09:39:36 +0900

    TextField, SecureField

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,35 +10,24 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
     @State private var message = ""
-    
     var body: some View {
-        
-        VStack (spacing: 28) {
-            Text(message)
+        VStack {
+            TextField("Place Holder", text: $message)
+                .textFieldStyle(.roundedBorder)
+                .disableAutocorrection(true) //자동 완성 끔->true
+                .textContentType(.emailAddress) //키보드 모양
+                .submitLabel(.done) // 자판기 완료 버튼부분 커스텀
+                .padding()
+                
             
-            Picker("Favorite Color", selection: $message, content: {
-                Text("Happy").tag("happy")
-                Text("Sad").tag("sad")
-                Text("Bored").tag("bored")
-            })
-            .pickerStyle(SegmentedPickerStyle())
-            .onChange(of: message) { newValue in
-                switch newValue {
-                case "happy":
-                    message = "Be happy and joyus"
-                case "sad":
-                    message = "Lige can be a struggle at times"
-                case "bored":
-                    message = "Look for your purpose"
-                default:
-                    break
-                }
-            }
+            SecureField("Password", text: $message)
+                .textFieldStyle(.roundedBorder)
+                .padding()
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
