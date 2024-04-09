1c06e630ba6d67e88561d80b04cd9aa4926ba3f2
SeongKookKIM <ksk8646@naver.com>
2024-04-09 10:18:04 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,36 +8,13 @@
 import SwiftUI
 
 
-@available(iOS 15.0, *)
+
 
 struct ContentView: View {
-    @State private var message = ""
-    @FocusState var dismissKeyboard: Bool
+
     var body: some View {
         VStack {
-            TextField("Place Holder", text: $message)
-                .textFieldStyle(.roundedBorder)
-                //자동 완성 끔->true
-                .autocorrectionDisabled(true)
-                //키보드 모양
-                .textContentType(.emailAddress)
-                // 자판기 완료 버튼부분 커스텀
-                .submitLabel(.done)
-                .padding()
-                // 실행종료시 포커스 날리기
-                .focused($dismissKeyboard)
             
-            
-            SecureField("Password", text: $message)
-                .textFieldStyle(.roundedBorder)
-                .padding()
-            
-            TextEditor(text: $message)
-                
-            
-            Button("Hide keyboard") {
-                dismissKeyboard = false
-            }
         }
     }
 }
