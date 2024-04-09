174827b492e40458bb037e2a73cc7f7ff0bc9129
SeongKookKIM <ksk8646@naver.com>
2024-04-09 14:16:07 +0900

    확대/축소 제스처 감지

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    @State private var temValue: CGFloat = 0
+    @State private var finalValue: CGFloat = 1
 
     var body: some View {
         VStack {
-           
+            Image(systemName: "star.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .scaleEffect(finalValue + temValue)
+                .gesture(MagnificationGesture().onChanged { amount in
+                    temValue = amount - 1
+                }.onEnded { amount in
+                    finalValue += temValue
+                    temValue = 0
+                })
         }
     }
 }
