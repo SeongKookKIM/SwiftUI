efd9c35e0da8568dd9deb39b2a385182d1c3c5d7
SeongKookKIM <ksk8646@naver.com>
2024-04-09 11:37:23 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,33 +11,14 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var myDate = Date.now
-    let dateRange: ClosedRange<Date> = {
-        let calendar = Calendar.current
-        let startComponents = DateComponents(year: 2024, month: 3, day: 25)
-        let endComponents = DateComponents(year: 2024, month: 9, day: 13)
-        return calendar.date(from: startComponents)!
-        ...
-        calendar.date(from:endComponents)!
-    }()
-    
-    let formatter = DateFormatter()
+
     
     
     var body: some View {
         VStack {
-            Text("Chosen date = \(formatter.string(from: myDate))")
-                .padding()
-            
-            DatePicker(selection: $myDate, in: dateRange, displayedComponents: [.date], label: {Text("date")})
-                .datePickerStyle(.graphical)
-                .padding()
+           
         }
-        .onAppear() {
-            formatter.locale = Locale(identifier: "ko_KR")
-            formatter.dateStyle = .long
-            formatter.timeStyle = .medium
-        }
+        
     }
 }
 
