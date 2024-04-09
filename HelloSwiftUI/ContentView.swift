9a1b11576dcebc4e18f01adb001b33f8b4debfc3
SeongKookKIM <ksk8646@naver.com>
2024-04-09 15:02:24 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,21 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var degree = 0.0
+    
 
     var body: some View {
         VStack {
-            Image(systemName: "star.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .rotationEffect(Angle.degrees(degree))
-                .gesture(
-                        RotationGesture()
-                            .onChanged { angle in
-                                degree = angle.degrees
-                            }
-                )
+        
         }
     }
 }
