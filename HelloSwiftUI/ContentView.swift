fb51725826ddb3eae89b459021035edfab990449
SeongKookKIM <ksk8646@naver.com>
2024-04-09 15:44:39 +0900

    ContextMenu 기능추가

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,23 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+        @State var text = "길게 눌러 확인"
     var body: some View {
         VStack {
            Text("Turtle Rock")
                 .padding()
                 .contextMenu {
                     Button{
-                        
+                        text = "Favorite"
                     } label: {
                         Label("Add to Favoriate", systemImage: "heart")
                     }
                     Button{
-                        
+                        text = "Maps"
                     } label: {
                         Label("Show in Maps", systemImage: "mappin")
                     }
                 }
+            
+            Text("\(text)")
         }
     }
 }
