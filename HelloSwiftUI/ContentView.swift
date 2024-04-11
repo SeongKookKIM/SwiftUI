892e34dc2948bb6ed6daaa88b326dfde48e0f7bd
SeongKookKIM <ksk8646@naver.com>
2024-04-11 09:18:33 +0900

    DiscolsureGroup-Accordian

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,21 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    @State var sliderValue = 0.0
+    @State var message = ""
+    @State var flag = true
+    
     var body: some View {
         VStack {
-           
+            DisclosureGroup("펼치기") {
+                Text("입력한 내용 = \(message)")
+                TextField("Type here", text: $message)
+                Text(flag ? "Toggle = true" : "Toggle = false")
+                Toggle(isOn: $flag) {
+                    Text("Toggle")
+                }
+                Text("The slider value = \(sliderValue)")
+                Slider(value: $sliderValue, in: 0...15)
+                    .padding()
+            }
+            .padding()
         }
     }
 }
 
+
+
+
+#Preview {
+    ContentView()
+}
 
 //struct ContentView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        ContentView()
 //    }
 //}
-
-#Preview {
-ContentView()
-}
