98fa245d57a878c86c1fdfa0ae613902728972f0
SeongKookKIM <ksk8646@naver.com>
2024-04-11 15:16:13 +0900

    TabView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -12,8 +12,27 @@ struct ContentView: View {
 
     
     var body: some View {
-        NavigationStack {
-
+        TabView {
+            Text("One")
+                .tabItem {
+                    Image(systemName: "heart.fill")
+                    Text("one")
+                }
+            Text("Two")
+                .tabItem {
+                    Image(systemName: "hare.fill")
+                    Text("two")
+                }
+            Text("Three")
+                .tabItem {
+                    Image(systemName: "tortoise.fill")
+                    Text("three")
+                }
+            Text("four")
+                .tabItem {
+                    Image(systemName: "folder.fill")
+                    Text("four")
+                }
         }
     }
 }
