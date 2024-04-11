ab8e09ab5015279974a24995eacac6e6290ce6d2
SeongKookKIM <ksk8646@naver.com>
2024-04-11 09:30:15 +0900

    ScrollView

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -11,8 +11,10 @@ struct ContentView: View {
 
     
     var body: some View {
-        VStack {
-         
+        ScrollView(Axis.Set.vertical, showsIndicators: true) {
+            ForEach(0..<50) {
+                Text("Item #\($0)")
+            }
         }
     }
 }
