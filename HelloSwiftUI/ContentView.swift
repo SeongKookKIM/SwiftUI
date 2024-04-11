0a343648fdb289cb0bad0d5baccd379d2f35370c
SeongKookKIM <ksk8646@naver.com>
2024-04-11 17:06:14 +0900

    ScaleEffect

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var move = true
+    @State var changeMe = true
     var body: some View {
-        VStack {
-            Text("A Text View")
-                .offset(x: move ? 100: 0, y: move ? 100 : 0)
-                .animation(.bouncy, value: move)
-            Toggle(isOn: $move, label: {
-                Text("Toggle Me")
-            })
-        }
+        Image(systemName: "tortoise.fill")
+            .font(.system(size: 100))
+            .foregroundStyle(.red)
+            .scaleEffect(changeMe ? 1.75 : 1)
+            .animation(.default, value: changeMe)
+            .onTapGesture {
+                changeMe.toggle()
+            }
     }
 }
 
