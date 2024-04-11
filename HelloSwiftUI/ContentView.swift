1df125cb7e590834a5e5ed4378c9128cc1a9b280
SeongKookKIM <ksk8646@naver.com>
2024-04-11 11:00:52 +0900

    navigation

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -10,9 +10,38 @@ import SwiftUI
 
 
 struct ContentView: View {
-
+    @State var flag = true
+    @State var message = ""
     var body: some View {
-        
+        NavigationStack {
+            Text(message)
+            
+            Toggle(isOn: $flag, label: {
+                Text("Toggle displayMode")
+            })
+            .navigationTitle("Navigation Title")
+            .navigationBarHidden(!flag)
+            .navigationBarTitleDisplayMode(flag ? .large : .inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        message = "iCloud 아이콘 탭됨"
+                    } label: {
+                        Image(systemName: "icloud")
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        message = "완료 버튼 탭됨"
+                    } label: {
+                        Text("완료")
+                    }
+                }
+            }
+        }
+        .accentColor(.purple)
     }
 }
 
