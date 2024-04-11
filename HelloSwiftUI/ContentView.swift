aa399c50abdec916b86447e1fdeb18d9d633b431
SeongKookKIM <ksk8646@naver.com>
2024-04-11 09:08:20 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -8,48 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var flag = false
-    @State var message = ""
+
     var body: some View {
         VStack {
-            Form {
-                Section {
-                    Text("This is Section has no header")
-                }
-                Section("Just a Header") {
-                    Text("This Section uses a simple header")
-                }
-                Section {
-                    Text("This Section uses a simple footer")
-                } footer: {
-                    Text("Just a Footer")
-                }
-                Section {
-                    Text("This Section uses both a header and footer")
-                } header: {
-                    Text("The Header")
-                } footer: {
-                    Text("The Footer")
-                }
-            }
-            
-            Form {
-                Text("This is a Form")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-                //True 시 비활성화
-                .disabled(flag)
-            }
-            
-            
-            GroupBox(label: Text("Group Box")) {
-                Text("This is a Group box")
-                Toggle(isOn: $flag, label: {
-                    Text("Click me")
-                })
-                TextField("Type here", text: $message)
-            }
+           
         }
     }
 }
@@ -60,11 +22,7 @@ struct ContentView: View {
 //        ContentView()
 //    }
 //}
-/** IOS 17  부터 이 문법으로 바뀜...
- #Preview {
- ContentView()
- }
- **/
+
 #Preview {
 ContentView()
 }
