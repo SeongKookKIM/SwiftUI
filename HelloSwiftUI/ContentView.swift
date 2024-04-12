76f26537d437e44848ba5d03fc6df1158a3c2851
SeongKookKIM <ksk8646@naver.com>
2024-04-12 09:08:07 +0900

    Clear

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,32 +9,10 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var myDegress: Double = 0.0
-    @State var flag = false
+   
     var body: some View {
         VStack {
-            Text("Hello, World!")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegress : 0))
-//                .animation(.easeIn, value: flag)
-            Text("Hello, World!")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegress : 0))
-//                .animation(.easeOut, value: flag)
-            Text("Hello, World!")
-                .padding()
-                .rotationEffect(Angle(degrees: flag ? myDegress : 0))
-//                .animation(.easeInOut, value: flag)
-
-            Button("Amimate now") {
-                // 전체 애니메이션 트리거
-                withAnimation(.easeInOut(duration: 2.5)) {
-                    flag.toggle()
-                }
-                
-            }
-            Slider(value: $myDegress, in: -180...180, step: 1)
-                .padding()
+          
         }
     }
 }
